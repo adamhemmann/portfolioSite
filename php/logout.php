cba63df8f40a00a87fd834c765c94c17e5b5fca3
adamhemmann <adam.hemmann@gmail.com>
2021-12-14 13:49:40 -0600

    First commit

---
+++ php/logout.php
@@ -0,0 +1,23 @@
+<?php
+// Created by Professor Wergeles for CS2830 at the University of Missouri
+	
+    if (!session_start()) {
+        header("Location: error.php");
+        exit;
+    }
+
+    $_SESSION = array();
+    
+    // If the session was propagated using a cookie, remove that cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', 1,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    session_destroy();
+    header("Location: login.php");
+	exit;
+?>
